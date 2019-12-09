9333187d641fdfa9e8f038acd01833e1a40b1fdb
Sijey <Popov040391@gmail.com>
2019-12-09 12:56:03 +0200

    Updates

---
+++ precache-manifest.b92902c7ba5ea4e0cf510d11e0689a84.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b17facd3a8d3d55f90522098550bc5d4",
+    "url": "/weather-test-app/index.html"
+  },
+  {
+    "revision": "074f14ddd4a1330df7e8",
+    "url": "/weather-test-app/static/css/main.b9ceff5d.chunk.css"
+  },
+  {
+    "revision": "f662419d15f9b213e597",
+    "url": "/weather-test-app/static/js/2.7d1b4668.chunk.js"
+  },
+  {
+    "revision": "074f14ddd4a1330df7e8",
+    "url": "/weather-test-app/static/js/main.2a6d752f.chunk.js"
+  },
+  {
+    "revision": "613a462705e5c6120b94",
+    "url": "/weather-test-app/static/js/runtime-main.17280e3c.js"
+  }
+]);
